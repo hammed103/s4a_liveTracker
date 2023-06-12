f4767fff9fc08b99da35e615752685965b156f48
hammed103 <hammedfree@gmail.com>
2023-06-12 15:07:33 +0100

    refresh

--- live_tracker/urls.py
+++ live_tracker/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from live_tracker.views import UploadView,UploadPlay
+from live_tracker.views import UploadView, UploadPlay, refresh
 from live_tracker import views
 
 urlpatterns = [
     path("", views.HomePage, name="home"),
     path("playlist", views.Playlist, name="playlist"),
     path("api/upload-audio", UploadView.as_view()),
-    path("api/upload-play",UploadPlay.as_view())
+    path("api/upload-play", UploadPlay.as_view()),
+    path("api/refresh", refresh.as_view()),
 ]
