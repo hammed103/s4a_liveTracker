9b306a330fd1fb815a8307aa0046456a85ef60c3
hammed103 <hammedfree@gmail.com>
2023-06-12 18:06:12 +0100

    refreshMain

--- live_tracker/urls.py
+++ live_tracker/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from live_tracker.views import UploadView, UploadPlay, refresh
+from live_tracker.views import UploadView, UploadPlay, refresh, refreshMain
 from live_tracker import views
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path("api/upload-audio", UploadView.as_view()),
     path("api/upload-play", UploadPlay.as_view()),
     path("api/refresh", refresh.as_view()),
+    path("api/refreshMain", refreshMain.as_view()),
 ]
--- live_tracker/views.py
+++ live_tracker/views.py
@@ -13,33 +13,51 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import pygsheets
 
+
 def extract_artist_id(url):
     # Split the URL by "/"
     url_parts = url.split("/")
-    
+
     # Find the index of "artist" in the URL
     artist_index = url_parts.index("artist")
-    
+
     # Extract the artist ID
     artist_id = url_parts[artist_index + 1]
-    
+
     return artist_id
+
 
 def extract_playlist_id(url):
     # Split the URL by "/"
     url_parts = url.split("/")
-    
+
     # Find the index of "artist" in the URL
     artist_index = url_parts.index("playlist")
-    
+
     # Extract the artist ID
     artist_id = url_parts[artist_index + 1]
-    
+
     return artist_id
 
 
-
 # Example usage
+slack_token = "xoxb-907605934689-5286686717863-8EN26FvWuun1C8M8ZkUe7uZj"
+channel_id = "C05901NKQSY"
+
+art = [
+    ("4JoVRTzdITLFtxPx72MaQ5", "SPED UP DRILL GATES"),
+    ("4anRsJeihT0h3v3YO2q8wQ", "DRILL GATES"),
+    ("3EYY5FwDkHEYLw5V86SAtl", "SICK LEGEND"),
+    ("0aUMVkR8QV0LSdv9VZOATn", "TEKKNO"),
+    ("4YYOTpMoikKdYWWuTWjbqo", "HYPERTECHNO"),
+    ("3nZ3AabGubephVrPTp8rRz", "TECHNO N TEQUILLA"),
+    ("2MDj296KJIfgWDNBtHzeFi", "11:11 Music Group"),
+    ("6pOqcPiFkbjIKUBF86cfuM", "TWO PUNKS IN LOVE"),
+    ("4rlJtMEpxuem6xZ9DPycFD", "RAINY JASPER"),
+    ("3vDFmwP5PXRqcAEd9acoNs", "AMBIENT JASPER"),
+    ("4KGcll2G3f04WMGTT19eyz", "90210"),
+    ("45u4hhyZlr11XAFqO74eTZ", "DEADBOY"),
+]
 
 
 def colnum_to_colname(colnum):
@@ -48,14 +66,18 @@ def colnum_to_colname(colnum):
         colnum, remainder = divmod(colnum - 1, 26)
         colname = chr(65 + remainder) + colname
     return colname
+
+
 def soup_from_html(html_string):
     soup = BeautifulSoup(html_string, "html.parser")
     return soup
+
 
 import pyairtable
 from pyairtable.formulas import match
 from datetime import date
 from slack_sdk import WebClient
+
 # Get today's date
 today = date.today()
 
@@ -71,7 +93,7 @@ from selenium.webdriver.chrome.options import Options
 from seleniumwire import webdriver as wirewebdriver
 from selenium.webdriver.chrome.service import Service
 
-service = Service(executable_path='chromedriver')
+service = Service(executable_path="chromedriver")
 
 options = {
     "verify_ssl": True  # Verify SSL certificates but beware of errors with self-signed certificates
@@ -80,17 +102,16 @@ options = {
 # Set up Chrome options
 chrome_options = Options()
 chrome_options.add_argument("--headless")  # Run Chrome in headless mode
-#chrome_profile_path = "/content/Default"
-chrome_options.add_argument("--user-data-dir=Default" )
+# chrome_profile_path = "/content/Default"
+chrome_options.add_argument("--user-data-dir=Default")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
 
-
-
-
 # Create a new instance of ChromeDriver
-driver = wirewebdriver.Chrome(service=service, options=chrome_options,seleniumwire_options=options)
+driver = wirewebdriver.Chrome(
+    service=service, options=chrome_options, seleniumwire_options=options
+)
 # Now you can use the `driver` object to interact with the browser and access the requests made
 driver.get("https://artists.spotify.com/c/artist/0aUMVkR8QV0LSdv9VZOATn/home")
 sleep(3)
@@ -124,42 +145,43 @@ import requests
 import base64
 from time import sleep
 
-def vio(playlist_id) :
-    client_id = '53fb1dbe5f42480ba654fcc3c7e168d6'
-    client_secret = '5c1da4cce90f410e88966cdfc0785e3a'
 
-    auth_str = client_id + ':' + client_secret
+def vio(playlist_id):
+    client_id = "53fb1dbe5f42480ba654fcc3c7e168d6"
+    client_secret = "5c1da4cce90f410e88966cdfc0785e3a"
+
+    auth_str = client_id + ":" + client_secret
     b64_auth_str = base64.b64encode(auth_str.encode()).decode()
 
     auth_options = {
-        'url': 'https://accounts.spotify.com/api/token',
-        'headers': {
-            'Authorization': 'Basic ' + b64_auth_str
-        },
-        'data': {
-            'grant_type': 'client_credentials'
-        }
+        "url": "https://accounts.spotify.com/api/token",
+        "headers": {"Authorization": "Basic " + b64_auth_str},
+        "data": {"grant_type": "client_credentials"},
     }
 
     response = requests.post(**auth_options)
     if response.status_code == 200:
-        token = response.json()['access_token']
+        token = response.json()["access_token"]
 
-    headers = {
-        'Authorization': f'Bearer {token}' # Replace with actual token
-    }
+    headers = {"Authorization": f"Bearer {token}"}  # Replace with actual token
 
-    response = requests.get(f'https://api.spotify.com/v1/playlists/{playlist_id}', headers=headers)
+    response = requests.get(
+        f"https://api.spotify.com/v1/playlists/{playlist_id}", headers=headers
+    )
 
     if response.status_code == 200:
         playlist_data = response.json()
         # Do something with the playlist data
     else:
-        print(f'Error: {response.status_code}')
+        print(f"Error: {response.status_code}")
 
-    cnt = (playlist_data["followers"]["total"],playlist_data["name"])
+    cnt = (playlist_data["followers"]["total"], playlist_data["name"])
 
-    return {"id": playlist_id , "name":playlist_data["name"], "total":playlist_data["followers"]["total"]}
+    return {
+        "id": playlist_id,
+        "name": playlist_data["name"],
+        "total": playlist_data["followers"]["total"],
+    }
 
 
 def HomePage(req):
@@ -168,6 +190,7 @@ def HomePage(req):
         "base/index.html",
     )
 
+
 def Playlist(req):
     return render(
         req,
@@ -175,15 +198,14 @@ def Playlist(req):
     )
 
 
-
 class UploadView(APIView):
     @staticmethod
     def post(req):
         aid = req.data["aid"]
         try:
-          aid = extract_artist_id(aid)
+            aid = extract_artist_id(aid)
         except:
-          pass
+            pass
         print(aid)
         rff = requests.get(f"https://open.spotify.com/artist/{aid}")
 
@@ -343,10 +365,12 @@ class UploadView(APIView):
         # Convert the number of columns into a column label
         last_column_label = colnum_to_colname(num_columns)
 
+        df["Total Amount"] = pd.DataFrame(
+            [f"=SUM(C{i+2}:{last_column_label}{i+2})" for i in range(wks.rows - 1)],
+            columns=["Total Amount"],
+        )
 
-        df["Total Amount"] = pd.DataFrame([f'=SUM(C{i+2}:{last_column_label}{i+2})' for i in range(wks.rows-1)], columns=['Total Amount'])
-
-        wks.set_dataframe(df, start="A1",extend=True)
+        wks.set_dataframe(df, start="A1", extend=True)
         try:
             for rr in response:
                 records = airtable.first(formula=match({"Date": rr["Date"]}))
@@ -371,7 +395,6 @@ class UploadView(APIView):
         print("Upload complete")
 
 
-
 class UploadPlay(APIView):
     @staticmethod
     def post(req):
@@ -382,21 +405,27 @@ class UploadPlay(APIView):
             aid = extract_playlist_id(aid)
         data = vio(aid)
         # Authenticate with Google Sheets
-        gc = pygsheets.authorize(service_file='my-project-1515950162194-ea018b910e23.json')
+        gc = pygsheets.authorize(
+            service_file="my-project-1515950162194-ea018b910e23.json"
+        )
 
         # Open the Google Spreadsheet using its title
-        spreadsheet = gc.open('Playlist Tracker')
+        spreadsheet = gc.open("Playlist Tracker")
 
         # Select the worksheet
         worksheet = spreadsheet.sheet1
 
         # Add new data
-        
-        new_row = [data['id'], data['name'], data['total'], 0]  # Assuming Last24Hours is 0 for new rows
+
+        new_row = [
+            data["id"],
+            data["name"],
+            data["total"],
+            0,
+        ]  # Assuming Last24Hours is 0 for new rows
 
         # Add the new row to the end of the sheet
         worksheet.append_table(new_row)
-
 
         return Response(
             {
@@ -408,29 +437,179 @@ class UploadPlay(APIView):
         print("Upload complete")
 
 
-
 class refresh(APIView):
     @staticmethod
-    def get(req):  
-        gc = pygsheets.authorize(service_file='my-project-1515950162194-ea018b910e23.json')
+    def get(req):
+        gc = pygsheets.authorize(
+            service_file="my-project-1515950162194-ea018b910e23.json"
+        )
 
         # Open the Google Spreadsheet using its title
-        spreadsheet = gc.open('Playlist Tracker')
+        spreadsheet = gc.open("Playlist Tracker")
 
         # Select the worksheet
         worksheet = spreadsheet.sheet1
 
         ll = worksheet.get_as_df()
-        for index,val in ll.iterrows() :
+        for index, val in ll.iterrows():
             data = vio(val.PlaylistId)
-            pix = (data["total"] - val.Total)
-            ll.loc[index,"Total"] = data["total"]
-            ll.loc[index,"Last24Hours"] = pix
+            pix = data["total"] - val.Total
+            ll.loc[index, "Total"] = data["total"]
+            ll.loc[index, "Last24Hours"] = pix
 
         worksheet.clear()
 
-        #worksheet.set_dataframe(ll, start="A1",extend=True)
-        
+        # worksheet.set_dataframe(ll, start="A1",extend=True)
+
+        return Response(
+            {
+                "status": "success",
+            },
+            status=201,
+        )
+        print("Upload complete")
+
+
+class refreshMain(APIView):
+    @staticmethod
+    def get(req):
+
+        global driver
+        for request in driver.requests:
+            if request.headers:
+                if "authorization" in request.headers:
+                    auth_header = request.headers["Authorization"]
+                    if auth_header != "":
+                        break
+
+        print("Authorization Header:", auth_header)
+        headers = {
+            "authority": "generic.wg.spotify.com",
+            "accept": "application/json",
+            "accept-language": "en-US",
+            "app-platform": "Browser",
+            "authorization": f"{auth_header}",
+            "content-type": "application/json",
+            "origin": "https://artists.spotify.com",
+            "referer": "https://artists.spotify.com/",
+            "sec-ch-ua": '"Microsoft Edge";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Windows"',
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-site",
+            "spotify-app-version": "1.0.0.48e3603",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.35",
+            "x-cloud-trace-context": "00000000000000002a87751b4619e7dc/1588903106916990606;o=1",
+        }
+
+        table_name = "main"  # Replace with your Airtable table name
+
+        artx = art.copy()
+        artx.remove(artx[1])
+        artx.remove(artx[4])
+        params = {
+            "aggregation-level": "recording",
+            "time-filter": "last5years",
+        }
+        # Listeners Summary
+        for topic in ["listeners", "Streams", "Followers"]:
+            table_name = f"{topic} Summary"  # Replace with your Airtable table name
+
+            airtable = pyairtable.Table(api_key, base_id, table_name)
+
+            slack_token = "xoxb-907605934689-5286686717863-8EN26FvWuun1C8M8ZkUe7uZj"
+            channel_id = "C05901NKQSY"
+
+            comb = []
+            for aid, anam in art:
+
+                response = requests.get(
+                    f"https://generic.wg.spotify.com/s4x-insights-api/v1/artist/4YYOTpMoikKdYWWuTWjbqo/audience/timeline/{topic.lower()}/{aid}",
+                    params=params,
+                    headers=headers,
+                )
+                HYPERTECHNO = response.json()["timelinePoint"][:143]
+
+                # Iterate over each dictionary in the list
+                # Example list of dictionaries
+
+                # Define a dictionary mapping old keys to new keys
+                key_mapping = {"date": "Date", "num": f"{anam}"}
+
+                # Create a new list of dictionaries with renamed keys
+                new_HYPERTECHNO = [
+                    {key_mapping.get(key, key): value for key, value in item.items()}
+                    for item in HYPERTECHNO
+                ]
+
+                # Print the updated list of dictionaries
+
+                # Define the keys to convert from string to integer
+                keys_to_convert = [f"{anam}"]
+
+                # Convert string values to integers using list comprehension
+                new_HYPERTECHNO = [
+                    {
+                        key: int(value) if key in keys_to_convert else value
+                        for key, value in item.items()
+                    }
+                    for item in new_HYPERTECHNO
+                ]
+
+                # Print the updated dictionary
+                comb.append(new_HYPERTECHNO)
+            comb = [i for j in comb for i in j]
+            # Merge dictionaries based on 'Date'
+            merged_dict = {}
+
+            for item in comb:
+                date = item["Date"]
+                if date not in merged_dict:
+                    merged_dict[date] = {}
+
+                for key, value in item.items():
+                    if key != "Date":
+                        merged_dict[date][key] = value
+
+            # Convert merged_dict to a list of dictionaries
+            merged_list = [{"Date": key, **value} for key, value in merged_dict.items()]
+
+            # Print the merged list
+            print(merged_list)
+
+            existing_records = airtable.all()
+
+            record_exists = False
+
+            bobo = [i["fields"]["Date"] for i in existing_records]
+            for record in merged_list:
+
+                if record["Date"] in bobo:
+                    pass
+                else:
+
+                    print(f"Update made for {record['Date'] }")
+                    # Insert the new record at the top
+                    airtable.create(
+                        record,
+                    )
+                    upx = record
+                    client = WebClient(token=slack_token)
+
+                    message = f"*{topic.upper()} New Record*\n"
+                    max_key_length = max(len(key) for key in upx.keys()) + 6
+
+                    for key, value in upx.items():
+                        if key == "Date":
+                            message += (
+                                f":date: *{key.ljust(max_key_length)}*: *_{value}_*\n\n"
+                            )
+                        else:
+                            message += f":globe_with_meridians: *{key.ljust(max_key_length)}*: *_{value}_*\n\n"
+
+                    response = client.chat_postMessage(channel=channel_id, text=message)
+
         return Response(
             {
                 "status": "success",
