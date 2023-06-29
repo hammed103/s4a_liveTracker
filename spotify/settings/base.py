4b2b4eb986df83e726ba4601556d232f0aec8f22
Your Name <hammedfree@gmail.com>
2023-06-29 04:39:15 +0000

    move

--- spotify/settings/base.py
+++ spotify/settings/base.py
@@ -82,14 +82,15 @@ WSGI_APPLICATION = "spotify.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'spotify',
+        'USER': 'postgres',
+        'PASSWORD': '1111',
+        'HOST': '35.202.13.40',  # Typically 'localhost' if running locally
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
