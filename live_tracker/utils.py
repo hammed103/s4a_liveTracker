25199dbd1b9c1893b46c09a38605fe3cf261cf80
hammed103 <hammedfree@gmail.com>
2023-06-29 10:18:43 +0100

    mover

--- live_tracker/utils.py
+++ live_tracker/utils.py
@@ -20,7 +20,6 @@ from datetime import date
 from slack_sdk import WebClient
 
 
-
 # selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -39,8 +38,8 @@ options = {
 # Set up Chrome options
 chrome_options = Options()
 chrome_options.add_argument("--headless")  # Run Chrome in headless mode
-chrome_profile_path = "/content/Default"
-chrome_options.add_argument("--user-data-dir=" +chrome_profile_path)
+chrome_profile_path = "/Default"
+chrome_options.add_argument("--user-data-dir=" + chrome_profile_path)
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
