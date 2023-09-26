cb4bbcd77f7dedb562eb2948baf7722d2c51ce5c
hammed103 <hammedfree@gmail.com>
2023-09-26 21:27:04 +0100

    mate

--- live_tracker/urls.py
+++ live_tracker/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
 from live_tracker.views import refreshMain,start
-from live_tracker import views
+#from live_tracker import views
 from .new_views import start
 urlpatterns = [
-    path("", views.HomePage, name="home"),
-    path("api/refreshMain", refreshMain.as_view()),
+    #path("", views.HomePage, name="home"),
+    #path("api/refreshMain", refreshMain.as_view()),
     path("api/start", start.as_view()),
 ]
