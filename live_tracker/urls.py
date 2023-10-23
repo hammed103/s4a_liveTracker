054cb006a72bdce9b18229d5e84b06691c1744a2
hammed103 <hammedfree@gmail.com>
2023-10-23 13:57:23 +0100

    mate

--- live_tracker/urls.py
+++ live_tracker/urls.py
@@ -2,13 +2,11 @@ from django.urls import path
 
 #from live_tracker.views import refreshMain,start
 #from live_tracker import views
-from .new_views import start,youtube,segment,demo,source
+from .new_views import start,youtube
 urlpatterns = [
     #path("", views.HomePage, name="home"),
     #path("api/refreshMain", refreshMain.as_view()),
     path("api/start", start.as_view()),
-    path("api/segment", segment.as_view()),
-     path("api/demo", demo.as_view()),
-     path("api/source", source.as_view()),
+    
     path("api/youtube", youtube.as_view()),
 ]
